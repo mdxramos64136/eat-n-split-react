226074024176bce1ef442327edc65bf5e13210c6
Marcel Ramos <marceldavid@hotmail.com>
2025-04-22 17:32:18 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
+      </div>
     </div>
   );
 }
+///////////////////////////////////////////////////////
+function FriendList() {
+  //array to be rendered
+  const friends = initialFriends;
+  //
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend
+          friendProp={friend}
+          key={friend.id}
+        />
+      ))}
+    </ul>
+  );
+}
+///////////////////////////////////////////////////////
+function Friend({ friendProp }) {
+  return (
+    <li>
+      <img
+        src={friendProp.image}
+        alt={friendProp.name}
+      />
+      <h3>{friendProp.name}</h3>
+      {friendProp.balance < 0 && (
+        <p className="red">
+          You owe {friendProp.name} C$ {Math.abs(friendProp.balance)}
+        </p>
+      )}
+      {friendProp.balance > 0 && (
+        <p className="green">
+          {friendProp.name} owes you C$ {Math.abs(friendProp.balance)}
+        </p>
+      )}
+      {friendProp.balance === 0 && <p>You and {friendProp.name} are even</p>}
+      <Button>Select</Button>
+    </li>
+  );
+}
+///////////////////////////////////////////////////////
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+///////////////////////////////////////////////////////
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>🧞Friend Name</label>
+      <input type="text" />
+      <label>📷Image URL</label>
+      <input type="text" />
+      <Button>Select</Button>
+    </form>
+  );
+}
+///////////////////////////////////////////////////////
+function FormSplitBill() {
+  return (
+    <form>
+      <h2>Split a bill with friend</h2>
+    </form>
+  );
+}
+///////////////////////////////////////////////////////
 
-export default App;
+///////////////////////////////////////////////////////
